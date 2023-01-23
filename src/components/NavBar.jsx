4a1706a9124a6b9799a4b472b03576900d686bd9
Kamran Fulleylove <kamran-j-fulleylove@Kamrans-MacBook-Pro.local>
2023-01-23 15:59:36 +1100

    added medi life logo and edited positioning

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './navbar.css'
+import {GiHospitalCross} from 'react-icons/gi'
 import { Link } from 'react-router-dom'
 
 
@@ -13,20 +14,24 @@ const NavBar = () => {
         <div className='row'>
             <div className='col-auto'>
                 <div className='col-auto min-vh-100 '>
+                    <div classname='header'>
+                           <GiHospitalCross className='header-img'/>
+                        <h3>Medi-Life</h3>
+                    </div>
                     <ul>
                         <ul>
-                            <a className='nav-link px-1 m-3 p-3'>
+                            <a className='nav-link px-1 m-1 p-3'>
                                 <i className='bi bi-house-door-fill' /> <Link to='/' className='ms-1 d-none d-sm-inline '>Home</Link>
                             </a>
                         </ul> 
                         <ul>
-                            <a className='nav-link px-1 m-3 p-3'>
+                            <a className='nav-link px-1 m-1 p-3'>
                                 <i className='bi-table' /> <Link to='/appointments' className='ms-1 d-none d-sm-inline'>Appointment</Link>
                             </a>
                         </ul> 
                         <ul>
                       
-                            <a className='nav-link px-1 m-3 p-3'>
+                            <a className='nav-link px-1 m-1 p-3'>
                                 <i className='bi bi-box-arrow-right' /> <Link to='/logout' className='ms-1 d-none d-sm-inline'>Logout</Link>
                                
                             </a>
@@ -36,7 +41,7 @@ const NavBar = () => {
             </div>
         </div>
     </div>
-             )
+    )
 }
 
 export default NavBar;
