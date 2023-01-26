d39b7a414401116dd490abc82ee6cad8cfd6a277
Erkan <12980@coderacademy.edu.au>
2023-01-26 21:48:46 +1100

    main small devices responsive added

--- src/pages/bookings/Bookings.jsx
+++ src/pages/bookings/Bookings.jsx
@@ -7,19 +7,19 @@ import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineTwitter } from 'react-i
 
 
 const Bookings = () => {
- 
+
   return (
     <>
-    <Layout/>
-    <div className='contact'>
-    <h1 className='social-heading'>Contact us on socials</h1>
-    <div className='socials-mainlayout'>  
-    <AiOutlineFacebook className='social-icons' />
-    <AiOutlineInstagram className='social-icons'/>
-    <AiOutlineTwitter className='social-icons'/> 
-    </div>
-    
-    </div>
+      <Layout />
+      <div className='contact'>
+        <h1 className='social-heading'>Contact us on socials</h1>
+        <div className='socials-mainlayout'>
+          <AiOutlineFacebook className='social-icons' />
+          <AiOutlineInstagram className='social-icons' />
+          <AiOutlineTwitter className='social-icons' />
+        </div>
+
+      </div>
     </>
   )
 }
