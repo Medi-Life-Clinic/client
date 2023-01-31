595a26eeadb00663ab524149928db18c8a3a41d3
ad0maa <adam.tunchay@me.com>
2023-01-31 16:01:58 +1100

    admin component added, rough in

--- src/components/AdminComponent.jsx
+++ src/components/AdminComponent.jsx
@@ -1,19 +1,63 @@
 import React from "react";
-
+import "../pages/admin/admin.css";
+import AppointmentsComponent from "./AppointmentsComponent";
 
 const AdminComponent = () => {
-    document.title = 'Medi-Life | Admin'
+  document.title = "Medi-Life | Admin";
+  const appointments = AppointmentsComponent.getAppointment()
+  console.log(appointments)
 
+// const appointments = appointments.AppointmentsComponent.getAppointment()
+console.log(appointments)
+  return (
+    <>
+      <div className="layout-header">
+        <h1>{location.pathname === "/admin" ? "Admin" : "Meet our doctors"}</h1>
+      </div>
+      <container className="admin-container">
+        <container className="users">
+          <h1>Users</h1>
+          <table>
+            <tr>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Delete</th>
+            </tr>
+            <tr>
+              <td>Adam T</td>
+              <td>adam.tunchay@me.com</td>
+              <td>
+                <input type="checkbox"></input>
+              </td>
+            </tr>
+          </table>
+        </container>
+        <container className="doctors">
+          <h1>Doctors</h1>
+          <table>
+            <tr>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Delete</th>
+            </tr>
+          </table>
+        </container>
+        <container className="appointments">
+          <h1>Appointments</h1>
+          <table>
+            <tr>
+              <th>Appointment Id</th>
+              <th>Doctor</th>
+              <th>User</th>
+              <th>DoctorInfo</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </table>
+        </container>
+      </container>
+    </>
+  );
+};
 
-
-    return(
-
-        <div className="admin-container">
-            <h1>Admin blah blah</h1>
-        </div>
-
-    )
-
-}
-
-export default AdminComponent
+export default AdminComponent;
