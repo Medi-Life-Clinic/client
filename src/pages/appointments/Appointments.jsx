4b2888ddf1ed14bb85235708157302b45d2fc090
Kamran Fulleylove <kamran-j-fulleylove@Kamrans-MacBook-Pro.local>
2023-02-01 10:23:27 +1100

    removed socials tags

--- src/pages/appointments/Appointments.jsx
+++ src/pages/appointments/Appointments.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Layout from '../../components/MainLayout'
 import { Link, useLocation } from 'react-router-dom'
 import { GiHospitalCross } from 'react-icons/gi'
-
-import Socials from '../../components/Socials'
 import './appointments.css'
 
 
@@ -15,7 +13,6 @@ const Appointments = () => {
     <>
     <Layout>
       
-      <Socials />
     </Layout>
     </>
   )
