0cd491c7e17530ccd4825d5c64fb138b90bca31f
Kamran Fulleylove <kamran-j-fulleylove@Kamrans-MacBook-Pro.local>
2023-02-02 18:53:26 +1100

    Updated tests

--- src/tests/Login.test.jsx
+++ src/tests/Login.test.jsx
@@ -5,6 +5,7 @@ import Login from '../components/Login';
 import Socials from '../components/Socials';
 import { BrowserRouter } from 'react-router-dom';
 
+
 //Passed Test 
 
 describe('Login socials', () => {
@@ -26,13 +27,7 @@ it('Should render password input', () => {
     const{ getByPlaceholderText } = render(<BrowserRouter><Login /></BrowserRouter>)
     expect(getByPlaceholderText(/Password/i)).toBeInTheDocument()
 })
-// //Failed
-// it('Should update the email state when the input value changes', () => {
-//     const { getByPlaceholderText } = render(<BrowserRouter><Login /></BrowserRouter>)
-//     const emailinput = getByPlaceholderText(/Email/i)
-//     fireEvent.change(emailInput, { target: { value: 'test@email.com'}})
-//     expect(emailInput.value).toBe('test@email.com')
-// })
+
 
 //Passed
 it('Should update the email state when input value changes', () => {
@@ -52,35 +47,18 @@ it('Should update the password state when input value changes', () => {
 
 
 
+describe('Register Component', () =>{
+    it('Should reroute to the register page', () => {
+        const { getByTestId } = render(
+          <BrowserRouter><Login /></BrowserRouter>
+        )
+        const registerLink = getByTestId('register-link')
+        fireEvent.click(registerLink)
+        const navigate = jest.fn()
+      
+        expect(navigate).toHaveBeenCalledWith('/register');
+      })
+    })
 
 
 
-
-
-
-// //Failed -Dep
-// it('Should display a toast notification when form submission fails', () => {
-//     const { getByPlaceholderText, getByText } = render(<Login />)
-//     const emailInput = getByPlaceholderText(/Email/i)
-//     fireEvent.change(emailInput, { target: { value: 'test@email.com' } })
-//     const passwordInput = getByPlaceholderText(/Password/i)
-//     fireEvent.change(passwordInput, { target: { value: 'testpassword' } })
-//     const submitBtn = getByText(/LOGIN/i)
-//     fireEvent.click(submitBtn)
-//     const toast = getByText(/Something went wrong/i)
-//     expect(toast).toBeInTheDocument()
-// })
-
-// //Failed - Dep Testing state of email and password field
-// describe('Test the state of the email field and password fields', () => {
-//     it('Displays the email box with an empty value', () => {
-//         const { getByLabelText } = render(<BrowserRouter><Login /></BrowserRouter>)
-//         const emailInput = getByLabelText(/email/i)
-//         expect(emailInput.value).toBe('')
-//     })
-//     it('Displays the password box with an empty value', () => {
-//         const { getByLabelText } = render(<BrowserRouter><Login /></BrowserRouter>)
-//         const emailInput = getByLabelText(/password/i)
-//         expect(passwordInput.value).toBe('')
-//     })
-// })
