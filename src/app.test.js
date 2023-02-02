6a69a47538f09881e00a92d3735f998076fb8907
Kamran Fulleylove <kamran-j-fulleylove@Kamrans-MacBook-Pro.local>
2023-02-02 15:53:48 +1100

    Fixed broken paths and reset up config, copying over my tests from old branch

--- src/app.test.js
+++
@@ -1,4 +0,0 @@
-describe("App tests", () => 
-{
-    
-})
---
+++ src/tests/App.test.jsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react"
+import App from "./src/App.jsx"
+
+
+
+describe("App Component", () => {
+    it('Renders the app',  () => {
+        render((<App />))
+
+})
+})
