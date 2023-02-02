5ed9e01a2c458aed894b4f3505da8dc7c3855637
Erkan <12980@coderacademy.edu.au>
2023-02-02 23:29:05 +1100

    added remaining CSS

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,15 +11,14 @@ const NavBar = ({ children }) => {
     // Save the username from local storage to the variable userName
     // displays username in the h4 tag below.
     const userName = localStorage.getItem("user")
-    const [showNav, setShowNav] = useState(false);
-
-
-
+    
 
     const isAdmin = () => {
         const role = localStorage.getItem("isAdmin")
+        const [showNav, setShowNav] = useState(false);
         // This if statement is admin if true
         if (role === "true") {
+            
             return (
                 <div className='sidebar'>
                     <div className="sidebar-header">
